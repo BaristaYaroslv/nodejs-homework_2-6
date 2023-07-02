baa9d3859c090de3331c1fbce1dc017865fa0075
Yaroslav <110727630+BaristaYaroslv@users.noreply.github.com>
2023-07-02 11:31:09 +0300

    Update validateBody.js

--- middleWares/validateBody.js
+++ middleWares/validateBody.js
@@ -3,12 +3,12 @@
 const validateBody = schema => {
   const func = (req, res, next) => {
     // if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
-    //   throw HttpError(400);
+    //   throw HttpError(400); Помилка від Joi або іншої бібліотеки валідації
     // }
     const { error } = schema.validate(req.body);
     if (error) {
-      // next(HttpError(400, error.message));
-      res.status(400).json({message: "Помилка від Joi або іншої бібліотеки валідації"});
+      // next(HttpError(400, error.message)); 
+      res.status(400).json({message: `${error.message}`});
     }
 
     next(error);
